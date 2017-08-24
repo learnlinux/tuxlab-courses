799f4a03497216035844385937caa041e1a112cb
Derek Brown <derek@allderek.com>
2017-08-23 21:01:31 -0400

    Added Course Basics

---
+++ courses/intro_to_git/cloning.js
@@ -0,0 +1,6 @@
+/**
+ **  TuxLab Course File
+ **  Generated Thu, 24 Aug 2017 01:01:13 GMT
+ **
+ **  Details at https://github.com/learnlinux/tuxlab-courses
+ **/
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+/**
+ **  TuxLab Community Courses
+ **  Gulpfile which compiles and packages TuxLab Community Courses
+ **  for distribuition.
+ **/
+
+  /* CONFIG */
+  const coursesDir = './courses/';
+  const jsComment =
+`/**
+ **  TuxLab Course File
+ **  Generated ${(new Date).toUTCString()}
+ **
+ **  Details at https://github.com/learnlinux/tuxlab-courses
+ **/
+`;
+
+  /* IMPORTS */
+  var fs = require('fs');
+  var path = require('path');
+
+  var gulp = require('gulp');
+  var gulpInsert = require('gulp-insert');
+  var gulpTypings = require('gulp-typings');
+  var gulpTypescript = require('gulp-typescript');
+
+  /* TASKS */
+  gulp.task('typings', function(){
+    return gulp.src("./typings.json")
+      .pipe(gulpTypings());
+  })
+
+  gulp.task('typescript', ['typings'], function(){
+
+      return gulp.src(coursesDir + '*/*.ts')
+
+       // Compile with Typescript
+       .pipe(gulpTypescript({
+         noImplicitAny: true,
+       })).js
+
+       // Append Warning Comment
+       .pipe(gulpInsert.prepend(jsComment))
+
+       // Pipe to Output
+       .pipe(gulp.dest(coursesDir))
+  })
+
+  gulp.task('default', ['typescript']);
